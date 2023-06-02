c31d94d000518e2adf97fd02d212458e26d8065b
Dhanesh kandan <93308457+dhaneshkandan@users.noreply.github.com>
2023-06-02 21:04:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -103,3 +103,10 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+/*==================== SHOW SCROLL Up ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
